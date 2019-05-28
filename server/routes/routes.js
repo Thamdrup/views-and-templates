d43de193f13009bec0be8a14cc01aedff52be7fa
Rasmus Thamdrup <5579327@rts-365.dk>
2019-05-28 09:30:25 +0200

    push

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,20 +1,23 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
       res.render('home', {
-         "titel":"min fancy website"
+         "titel":"min fancy website",
+         "page":"home"
       });
    });
 
    app.get('/product', (req, res, next) => {
       res.render('product', {
-         "titel": "Product site"
+         "titel": "Product site",
+         "page" : "product"
       });
       res.send("BROKEN SITE");
    });
 
    app.get('/contact', (req, res, next) => {
       res.render('contact', {
-         "titel": "Contact site"
+         "titel": "Contact site",
+         "page" : "contact"
       });
    });
 
